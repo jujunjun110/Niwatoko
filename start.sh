4d5708c79a809fe3aa3bbabe83b16739c12e1a38
jujunjun110 <jujunjun110@gmail.com>
2014-09-15 17:37:08 +0900

    Fix Mac環境でもLinux環境でも大丈夫に

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
-./vendor/julius-4.3.1/julius/julius.dSYM -C ./orders/orders.jconf -charconv EUC-JP UTF-8 -module &
+file="./vendor/julius-4.3.1/julius/julius"
+if [-f file]
+then
+	# linux environment
+	./vendor/julius-4.3.1/julius/julius -C ./orders/orders.jconf -charconv EUC-JP UTF-8 -module &
+else
+	# macintosh environment
+	./vendor/julius-4.3.1/julius/julius.dSYM -C ./orders/orders.jconf -charconv EUC-JP UTF-8 -module &
+fi
 sleep 5
 python niwatoko_controller.py
